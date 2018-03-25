4cb46de9ef9360a3f32a04614950673d1f4296e9
kse-PC\kse <graceee318@gmail.com>
2018-03-25 18:42:24 +0900

    quick sort

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,15 +1,38 @@
-
 public class QuickSort {
 	public static void quickSort(int[] array){
-		
+		quickSort(array, 0, array.length-1);
+	}
+	public static void quickSort(int[] array, int low, int high){
+		if(low < high){
+			int pivot = partition(array, low, high);
+			quickSort(array, low, pivot-1);
+			quickSort(array, pivot+1, high);
+		}
+	}
+	public static int partition(int[] array, int low, int high){
+		int pivot = array[high];
+		int smallerThanPivot = low-1;
+        for (int biggerThanPivot=low; biggerThanPivot<high; biggerThanPivot++)
+        {
+            if (array[biggerThanPivot] <= pivot)
+            {
+                smallerThanPivot++;
+                int temp = array[smallerThanPivot];
+                array[smallerThanPivot] = array[biggerThanPivot];
+                array[biggerThanPivot] = temp;
+            }
+        }
+ 
+        int temp = array[smallerThanPivot+1];
+        array[smallerThanPivot+1] = array[high];
+        array[high] = temp;
+        return smallerThanPivot+1;
 	}
 	public static void main(String[] args) {
 		int[] test = {5,3,6,1,0,2};
-		array = test;
-		mergeSort(array);
-		for(int i=0 ; i<array.length ; i++){
-			System.out.print(array[i]+" ");
+		quickSort(test);
+		for(int i=0 ; i<test.length ; i++){
+			System.out.print(test[i]+" ");
 		}
 	}
-
 }
