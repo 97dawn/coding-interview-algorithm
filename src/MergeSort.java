72f877797f7cae3a59e6b779fa3eebb91f62c6ad
kse-PC\kse <graceee318@gmail.com>
2018-03-25 17:06:29 +0900

    merge sort

---
+++ src/MergeSort.java
@@ -0,0 +1,61 @@
+
+public class MergeSort {
+	private static int[] temp;
+	private static int[] array;
+	public static void mergeSort(int[] array){
+		temp = new int[array.length];
+		divide(array, 0, array.length-1);
+	}
+	
+	private static void divide(int[] array, int leftStart, int rightEnd){
+		if(leftStart >= rightEnd) return;
+		int middle = (rightEnd + leftStart) / 2;
+		divide(array, leftStart, middle);
+		divide(array, middle+1, rightEnd);
+		merge(array, leftStart, middle, rightEnd);
+	}
+	
+	private static void merge(int[] array, int leftStart, int mid, int rightEnd){
+		for(int i=leftStart ; i<=rightEnd ; i++){
+			temp[i] = array[i];
+		}
+				
+		int left = leftStart;
+		int right = mid+1;
+		int index = leftStart;
+		while(left <= mid && right <= rightEnd){
+			if(temp[left] < temp[right]){
+				array[index] = temp[left];
+				left++;
+			}
+			else{
+				array[index] = temp[right];
+				right++;
+			}
+			index++;
+		}
+		if(left <= mid){
+			while(left <= mid){
+				array[index] = temp[left];
+				left++;
+				index++;
+			}
+		}
+		else{
+			while(right <= rightEnd){
+				array[index] = temp[right];
+				right++;
+				index++;
+			}
+		}
+	}
+	public static void main(String[] args) {
+		int[] test = {5,3,6,1,0,2};
+		array = test;
+		mergeSort(array);
+		for(int i=0 ; i<array.length ; i++){
+			System.out.print(array[i]+" ");
+		}
+	}
+
+}
